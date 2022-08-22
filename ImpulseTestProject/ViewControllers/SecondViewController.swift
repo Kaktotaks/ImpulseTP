fb3772797723691a6e020de1e9bf4b5afda5848e
Kaktotaks <leonidshevchenko350@gmail.com>
2022-08-22 11:34:25 +0300

    SecondViewController's flow - done.

--- ImpulseTestProject/ViewControllers/SecondViewController.swift
+++ ImpulseTestProject/ViewControllers/SecondViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-    
     // MARK: - Variables/Constants
     private lazy var image: UIImageView = {
         let image: UIImageView = .init()
@@ -17,9 +16,8 @@ class SecondViewController: UIViewController {
         image.translatesAutoresizingMaskIntoConstraints = false
         return image
     }()
-    
+    private lazy var currentPage = 0
     private lazy var scrolView = UIScrollView()
-    
     private lazy var pageControll: UIPageControl = {
         let pageControll = UIPageControl()
         pageControll.numberOfPages = 3
@@ -30,7 +28,6 @@ class SecondViewController: UIViewController {
         pageControll.translatesAutoresizingMaskIntoConstraints = false
         return pageControll
     }()
-    
     private lazy var nextButton: UIButton = {
         let nextButton = UIButton()
         nextButton.backgroundColor = .orange
@@ -42,7 +39,17 @@ class SecondViewController: UIViewController {
         nextButton.translatesAutoresizingMaskIntoConstraints = false
         return nextButton
     }()
-    
+    private lazy var continueButton: UIButton = {
+        let nextButton = UIButton()
+        nextButton.backgroundColor = .orange
+        nextButton.setTitleColor(.white, for: .normal)
+        nextButton.setTitle("Continue", for: .normal)
+        nextButton.titleLabel?.font = UIFont.systemFont(ofSize: CGFloat(16), weight: .semibold)
+        nextButton.layer.cornerRadius = 10
+        nextButton.addTarget(self, action: #selector(continueButtonPressed), for: .touchUpInside)
+        nextButton.translatesAutoresizingMaskIntoConstraints = false
+        return nextButton
+    }()
     private lazy var images = ["firstImage", "secondImage", "thirdImage"]
     private lazy var mainLabels = ["Boost Productivity", "Work Seamlessly", "Achieve Your Goals"]
     private lazy var labels = ["Take your productivity to the next level", "Get your work done seamlessly without interruption", "Boosted productivity will help you achieve the desired goals"]
@@ -52,6 +59,7 @@ class SecondViewController: UIViewController {
         super.viewDidLoad()
         scrolView.delegate = self
         navigationController?.isNavigationBarHidden = true
+        continueButton.isHidden = true
     }
     
     override func viewDidLayoutSubviews() {
@@ -64,6 +72,7 @@ class SecondViewController: UIViewController {
         view.addSubview(scrolView)
         view.addSubview(pageControll)
         view.addSubview(nextButton)
+        view.addSubview(continueButton)
         scrolView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height - (view.frame.height / 5))
         
         var constraints = [NSLayoutConstraint]()
@@ -77,6 +86,11 @@ class SecondViewController: UIViewController {
         constraints.append(nextButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: +40))
         constraints.append(nextButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40))
         constraints.append(nextButton.heightAnchor.constraint(equalToConstant: 52))
+        
+        constraints.append(continueButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -40))
+        constraints.append(continueButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: +40))
+        constraints.append(continueButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40))
+        constraints.append(continueButton.heightAnchor.constraint(equalToConstant: 52))
         
         NSLayoutConstraint.activate(constraints)
         
@@ -123,21 +137,49 @@ class SecondViewController: UIViewController {
         }
     }
     
+    @objc func nextButtonPressed() {
+        print("NextButton pressed")
+        if pageControll.currentPage == 0 {
+            currentPage = 0
+            currentPage += 1
+        } else if pageControll.currentPage == 1 {
+            currentPage = 1
+            currentPage += 1
+        } else {
+            currentPage = 2
+        }
+        pageControll.currentPage = currentPage
+        scrolView.setContentOffset(CGPoint(x: CGFloat(currentPage) * view.frame.size.width,
+                                           y: 0),
+                                   animated: true)
+    }
+    
+    @objc func continueButtonPressed() {
+        print("ContinueButton pressed")
+            let thirdVC = UINavigationController(rootViewController: ThirdViewController())
+            thirdVC.modalPresentationStyle = .popover
+            thirdVC.modalTransitionStyle = .coverVertical
+            present(thirdVC, animated: true)
+    }
+    
     @objc private func pageControllDidChanged(_ sender: UIPageControl) {
         let current = sender.currentPage
         scrolView.setContentOffset(CGPoint(x: CGFloat(current) * view.frame.size.width,
                                            y: 0),
                                    animated: true)
     }
-    
-    @objc func nextButtonPressed() {
-        print("NextButton pressed")
-        
-    }
 }
 
 extension SecondViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         pageControll.currentPage = Int(floor(Float(scrollView.contentOffset.x) / Float(scrollView.frame.size.width)))
+        
+        if pageControll.currentPage == 2 {
+            nextButton.isHidden = true
+            continueButton.isHidden = false
+        } else {
+            continueButton.isHidden = true
+            nextButton.isHidden = false
+        }
     }
 }
--- ImpulseTestProject/ViewControllers/ThirdViewController.swift
+++ ImpulseTestProject/ViewControllers/ThirdViewController.swift
@@ -9,8 +9,57 @@ import UIKit
 
 class ThirdViewController: UIViewController {
     
+    private lazy var containerView: UIView = {
+        let containerView = UIView()
+        containerView.backgroundColor = .darkGray
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.clipsToBounds = true
+        containerView.layer.cornerRadius = 30
+        return containerView
+    }()
+    
+    private lazy var continueButton: UIButton = {
+        let continueButton = UIButton()
+        continueButton.backgroundColor = .orange
+        continueButton.setTitleColor(.white, for: .normal)
+        continueButton.setTitle("Continue", for: .normal)
+        continueButton.titleLabel?.font = UIFont.systemFont(ofSize: CGFloat(16), weight: .semibold)
+        continueButton.layer.cornerRadius = 10
+        continueButton.addTarget(self, action: #selector(continueButtonPressed), for: .touchUpInside)
+        continueButton.translatesAutoresizingMaskIntoConstraints = false
+        return continueButton
+    }()
     
     
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.50)
+        setupView()
+    }
+    
+    func setupView(){
+        view.addSubview(containerView)
+        containerView.addSubview(continueButton)
+        
+        var constraints = [NSLayoutConstraint]()
+        constraints.append(containerView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor))
+        constraints.append(containerView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor))
+        constraints.append(containerView.widthAnchor.constraint(equalToConstant: view.frame.width - 30))
+        constraints.append(containerView.heightAnchor.constraint(equalToConstant: view.frame.height / 2))
+        
+        constraints.append(continueButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20))
+        constraints.append(continueButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20))
+        constraints.append(continueButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -20))
+        constraints.append(continueButton.heightAnchor.constraint(equalTo: containerView.heightAnchor, constant: -200))
+        
+
+        NSLayoutConstraint.activate(constraints)
+    }
+    
+    @objc func continueButtonPressed() {
+        print("continueButton in ThirdViewController Pressed")
+    }
     
     
 }
